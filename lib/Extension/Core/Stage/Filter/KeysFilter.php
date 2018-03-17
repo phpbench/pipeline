7369da9828a044179f634805271d45ab87713c61
dantleech <dan.t.leech@gmail.com>
2018-03-17 17:10:53 +0100

    Exception for invalid keys in filter

--- lib/Extension/Core/Stage/Filter/KeysFilter.php
+++ lib/Extension/Core/Stage/Filter/KeysFilter.php
@@ -5,6 +5,7 @@ namespace PhpBench\Pipeline\Extension\Core\Stage\Filter;
 use PhpBench\Pipeline\Core\Stage;
 use Generator;
 use PhpBench\Pipeline\Core\Schema;
+use InvalidArgumentException;
 
 class KeysFilter implements Stage
 {
@@ -13,6 +14,13 @@ class KeysFilter implements Stage
         list($config, $data) = yield;
 
         while (true) {
+            if ($diff = array_diff($config['keys'], array_keys($data))) {
+                throw new InvalidArgumentException(sprintf(
+                    'Data does not contain key(s) "%s", it contains keys: "%s"',
+                    implode('", "', $diff), implode('", "', array_keys($data))
+                ));
+            }
+
             list($config, $data) = yield array_filter($data, function ($key) use ($config) {
                 return in_array($key, $config['keys']);
             }, ARRAY_FILTER_USE_KEY);
--- tests/Unit/Extension/Core/Stage/Filter/KeysFilterTest.php
+++ tests/Unit/Extension/Core/Stage/Filter/KeysFilterTest.php
@@ -3,6 +3,7 @@
 namespace PhpBench\Pipeline\Tests\Unit\Extension\Core\Stage\Filter;
 
 use PhpBench\Pipeline\Tests\Unit\Extension\Core\CoreTestCase;
+use InvalidArgumentException;
 
 class KeysFilterTest extends CoreTestCase
 {
@@ -21,5 +22,18 @@ class KeysFilterTest extends CoreTestCase
             'two' => 2,
             'three' => 3,
         ], $result);
+    }
+
+    public function testThrowsExceptionIfKeyDoesNotExist()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Data does not contain key(s) "three", it contains keys: "one", "two"');
+
+        $result = $this->pipeline()->stage('filter/keys', ['keys' => ['one', 'two', 'three']])
+            ->generator([
+                'one' => 1,
+                'two' => 2,
+            ])
+            ->current();
     }
 }
